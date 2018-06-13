bd141bbf0ca12f164d0f12288f1e7c1c43319432
RuMaxwell <935906960@qq.com>
2018-06-13 20:25:20 +0800

    Solve the problem of construct the type of a type parameter
    It's like implementing the feature of new constraint in C#.
    Did solve the problem, but still having looping construction problem, which also can't be solved in C#.
    
    Attempt of step-02 cleared.
    Discoveries:
    - class Foo<T> {
    	  data: T;
          constructor ( creator: { new (...args: any[]): T }, ...args ) {
    		  data = new creator(args);
          }
      }
    - The way of implementing a container (like the binary tree or anything else that needs a null type and a containing type) in step-02 is inproper. The right way is like in Cool. See further commits.

--- step-02.js
+++ step-02.js
@@ -6,58 +6,59 @@ var Ordering;
 })(Ordering || (Ordering = {}));
 function repeat(n, s) {
     var res = "";
-    'a' === 'a';
     for (var i = 0; i < n; i++) {
         res += s;
     }
     return res;
 }
-function showHelper(t, x) {
-    if (t.isNull()) {
-        return t.show();
-    }
-    else {
-        return "\n" + repeat(x, "  ") + "(" + t.val().show()
-            + showHelper(t.left(), x + 1) + showHelper(t.right(), x + 1) + ")";
-    }
-}
-var NullTree = /** @class */ (function () {
-    function NullTree() {
-    }
-    NullTree.prototype.isNull = function () {
-        return true;
-    };
-    NullTree.prototype.val = function () { };
-    NullTree.prototype.left = function () { };
-    NullTree.prototype.right = function () { };
-    NullTree.prototype.show = function () {
-        return " *";
-    };
-    return NullTree;
-}());
 var Tree = /** @class */ (function () {
-    function Tree(value, leftnode, rightnode) {
-        this.value = value;
-        this.leftnode = leftnode;
-        this.rightnode = rightnode;
+    function Tree(creator // A constructor of T, used to initialize this.value when no value is passed in
+    , value // The node value
+    , leftnode // Left subtree
+    , rightnode // Right subtree
+    ) {
+        var args = []; // The constructor parameters of T
+        for (var _i = 4 // The constructor parameters of T
+        ; _i < arguments.length // The constructor parameters of T
+        ; _i++ // The constructor parameters of T
+        ) {
+            args[_i - 4] = arguments[_i]; // The constructor parameters of T
+        }
+        this.isNull = true;
+        if (value !== undefined) {
+            this.isNull = false;
+            this.value = value;
+        }
+        else {
+            this.value = new creator(args);
+        }
+        if (leftnode !== undefined) {
+            this.leftChild = leftnode;
+        }
+        else {
+            this.leftChild = nullTree(creator);
+        }
+        if (rightnode !== undefined) {
+            this.rightChild = rightnode;
+        }
+        else {
+            this.rightChild = nullTree(creator);
+        }
     }
-    Tree.prototype.isNull = function () {
-        return false;
-    };
-    Tree.prototype.val = function () {
-        return this.value;
-    };
-    Tree.prototype.left = function () {
-        return this.leftnode;
-    };
-    Tree.prototype.right = function () {
-        return this.rightnode;
-    };
     Tree.prototype.show = function () {
         return showHelper(this, 0);
     };
     return Tree;
 }());
+function showHelper(t, x) {
+    if (t.isNull) {
+        return t.show();
+    }
+    else {
+        return "\n" + repeat(x, "  ") + "(" + t.value.show()
+            + showHelper(t.leftChild, x + 1) + showHelper(t.rightChild, x + 1) + ")";
+    }
+}
 var MyNumber = /** @class */ (function () {
     function MyNumber(value) {
         this.value = value;
@@ -73,7 +74,7 @@ var MyNumber = /** @class */ (function () {
     };
     MyNumber.prototype.fromList = function (ns) {
         if (ns.length === 0) {
-            return nullTree();
+            return [];
         }
         else {
             return [new MyNumber(ns[0])].concat(this.fromList(ns.slice(1)));
@@ -81,36 +82,36 @@ var MyNumber = /** @class */ (function () {
     };
     return MyNumber;
 }());
-function insert(t, value) {
-    if (t.isNull()) {
-        return singleton(value);
+function insert(creator, t, value) {
+    if (t.isNull) {
+        return singleton(creator, value);
     }
     else {
-        if (value.equalTo(t.val())) {
+        if (value.equalTo(t.value)) {
             return t;
         }
-        else if (value.greaterThan(t.val())) {
-            return insert(t.right(), value);
+        else if (value.greaterThan(t.value)) {
+            return insert(creator, t.rightChild, value);
         }
         else {
-            return insert(t.left(), value);
+            return insert(creator, t.leftChild, value);
         }
     }
 }
-function nullTree() {
-    return new NullTree();
+function nullTree(creator) {
+    return new Tree(creator);
 }
-function singleton(value) {
-    return new Tree(value, nullTree(), nullTree());
+function singleton(creator, value) {
+    return new Tree(creator, value, nullTree(creator), nullTree(creator));
 }
 function putStrLn(s) {
     console.log(s + "\n");
 }
-function print(o) {
+function print_(o) {
     putStrLn(o.show());
 }
 function main(args) {
-    var t = insert(nullTree(), new MyNumber(5));
-    print(t);
+    var t = nullTree(MyNumber);
+    print_(t);
 }
 main();
--- step-02.ts
+++ step-02.ts
@@ -4,6 +4,7 @@ enum Ordering {
     GT
 }
 
+
 interface Eq<T> {
     equalTo: (r: T) => boolean;
 }
@@ -18,76 +19,72 @@ interface Show<T> {
     show: () => string;
 }
 
+
 function repeat(n: number, s: string) : string {
     let res = "";
-	'a' === 'a';
     for (let i = 0; i < n; i++) {
         res += s;
     }
     return res;
 }
 
-function showHelper<T extends Show<T>>(t: TreeType<T>, x: number) : string {
-    if (t.isNull()) {
-        return t.show();
-    }
-    else {
-        return "\n" + repeat(x, "  ") + "(" + t.val().show()
-            + showHelper(t.left(), x + 1) + showHelper(t.right(), x + 1) + ")";
-    }
+
+interface TreeType<T extends Show<T>> extends Show<TreeType<T>> {
+	isNull: boolean;
+	value: T;
+	leftChild: TreeType<T>;
+	rightChild: TreeType<T>;
 }
 
-interface TreeType<T> {
-	isNull: () => boolean;
-	val: () => T?;
-	left: () => TreeType<T>?;
-	right: () => TreeType<T>?;
-}
 
-class NullTree<T> implements Show<NullTree>, TreeType<T> {
-	constructor () {
-    }
+class Tree<T extends Show<T>> implements TreeType<T> {
+    public isNull: boolean = true;
+    public value: T;
+    public leftChild: TreeType<T>;
+    public rightChild: TreeType<T>;
 
-	isNull () {
-		return true;
-	}
-
-	val () {}
-	left () {}
-	right () {}
-
-    show () {
-        return " *"
-    }
-}
-
-class Tree<T extends Show<T>> implements Show<Tree<T>>, TreeType<T> {
-    constructor ( public value: T
-                , public leftnode: TreeType<T>
-                , public rightnode: TreeType<T>
+    constructor ( creator: { new (...args: any[]): T }      // A constructor of T, used to initialize this.value when no value is passed in
+                , value?: T                                 // The node value
+                , leftnode?: TreeType<T>                    // Left subtree
+                , rightnode?: TreeType<T>                   // Right subtree
+                , ...args: any[]                            // The constructor parameters of T
                 ) {
-	}
-
-	isNull () {
-		return false;
-	}
-
-	val () {
-		return this.value;
-	}
-
-	left () {
-		return this.leftnode;
-	}
-
-	right () {
-		return this.rightnode;
-	}
+        if (value !== undefined) {
+            this.isNull = false;
+            this.value = value;
+        }
+        else {
+            this.value = new creator(args);
+        }
+        if (leftnode !== undefined) {
+            this.leftChild = leftnode;
+        }
+        else {
+            this.leftChild = nullTree(creator);
+        }
+        if (rightnode !== undefined) {
+            this.rightChild = rightnode;
+        }
+        else {
+            this.rightChild = nullTree(creator);
+        }
+    }
 
     show (): string {
         return showHelper(this, 0);
     }
 }
+
+function showHelper<T extends Show<T>>(t: TreeType<T>, x: number) : string {
+    if (t.isNull) {
+        return t.show();
+    }
+    else {
+        return "\n" + repeat(x, "  ") + "(" + t.value.show()
+            + showHelper(t.leftChild, x + 1) + showHelper(t.rightChild, x + 1) + ")";
+    }
+}
+
 
 class MyNumber implements Ord<MyNumber>, Show<MyNumber> {
     constructor ( public value: number ) {
@@ -105,9 +102,9 @@ class MyNumber implements Ord<MyNumber>, Show<MyNumber> {
         return this.value.toString();
     }
 
-	fromList (ns: number[]) {
+	fromList (ns: number[]): MyNumber[] {
 		if (ns.length === 0) {
-			return nullTree();
+			return [];
 		}
 		else {
 			return [new MyNumber(ns[0]), ...this.fromList(ns.slice(1))];
@@ -115,42 +112,43 @@ class MyNumber implements Ord<MyNumber>, Show<MyNumber> {
 	}
 }
 
-function insert<T extends Show<T> & Ord<T>>(t: TreeType<T>, value: T): TreeType<T> {
-    if (t.isNull()) {
-        return singleton(value);
+
+function insert<T extends Show<T> & Ord<T>>(creator: { new (...args: any[]): T }, t: TreeType<T>, value: T): TreeType<T> {
+    if (t.isNull) {
+        return singleton(creator, value);
     }
     else {
-        if (value.equalTo(t.val())) {
+        if (value.equalTo(t.value)) {
             return t;
         }
-        else if (value.greaterThan(t.val())) {
-            return insert(t.right(), value);
+        else if (value.greaterThan(t.value)) {
+            return insert(creator, t.rightChild, value);
         }
         else {
-            return insert(t.left(), value);
+            return insert(creator, t.leftChild, value);
         }
     }
 }
 
-function nullTree<T extends Show<T>>(): TreeType<T> {
-    return new NullTree();
+function nullTree<T extends Show<T>>(creator: { new (...args: any[]): T }): TreeType<T> {
+    return new Tree<T>(creator);
 }
 
-function singleton<T extends Show<T>>(value: T) {
-    return new Tree<T>(value, nullTree(), nullTree());
+function singleton<T extends Show<T>>(creator: { new (...args: any[]): T }, value: T) {
+    return new Tree<T>(creator, value, nullTree(creator), nullTree(creator));
 }
 
 function putStrLn(s: string): void {
     console.log(s + "\n");
 }
 
-function print<T extends Show<T>>(o: T) {
+function print_<T extends Show<T>>(o: T) {
     putStrLn(o.show());
 }
 
-function main(args: string[]) {
-    let t = insert(nullTree<MyNumber>(), new MyNumber(5));
-    print(t);
+function main(args?: string[]) {
+    let t = nullTree(MyNumber);
+    print_(t);
 }
 
 main();
